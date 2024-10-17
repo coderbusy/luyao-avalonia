1c8ed0f1110bb29d1247bda300b4a936441a64bc
Soar360 <Soar360@live.com>
2024-10-17 13:27:33 +0800

    LuYao.Avalonia.Fonts.MiSans.Regular

---
+++ src/LuYao.Avalonia.Fonts.MiSans.Regular/AppBuilderExtension.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+
+namespace LuYao.Avalonia.Fonts.MiSans.Regular;
+
+public static class AppBuilderExtension
+{
+    public static AppBuilder WithMiSansRegularFont(this AppBuilder appBuilder)
+    {
+        return appBuilder.ConfigureFonts(fontManager =>
+        {
+            fontManager.AddFontCollection(new MiSansRegularFontCollection());
+        });
+    }
+}
---
+++ src/LuYao.Avalonia.Fonts.MiSans.Regular/MiSansRegularFontCollection.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Media.Fonts;
+using System;
+
+namespace LuYao.Avalonia.Fonts.MiSans.Regular;
+
+public sealed class MiSansRegularFontCollection : EmbeddedFontCollection
+{
+    public MiSansRegularFontCollection() : base(
+        new Uri("fonts:MiSans", UriKind.Absolute),
+        new Uri("avares://LuYao.Avalonia.Fonts.MiSans.Regular/Fonts", UriKind.Absolute)
+        )
+    {
+
+    }
+}
