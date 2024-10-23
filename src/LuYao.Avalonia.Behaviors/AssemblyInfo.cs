72b768d9020b38c5079a677226fcf2e5ce893cfa
Soar360 <Soar360@live.com>
2024-10-23 17:36:56 +0800

    AssemblyInfo

---
+++ src/LuYao.Avalonia.Behaviors/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Avalonia.Metadata;
+
+[assembly: XmlnsPrefix("https://www.coderbusy.com/luyao", "ly")]
+[assembly: XmlnsDefinition("https://www.coderbusy.com/luyao", "LuYao.Avalonia.Behaviors")]
