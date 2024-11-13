545c83fc2c40278a525332e58d4be35b10c7cdde
Soar360 <Soar360@live.com>
2024-11-13 16:54:49 +0800

    add demo

---
+++ demo/LuYao.Avalonia.Demo/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace LuYao.Avalonia.Demo.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ demo/LuYao.Avalonia.Demo/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace LuYao.Avalonia.Demo.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
