c1b65b335abf09d6a470d14a005cc76c91cf8b7e
Soar360 <soar360@live.com>
2025-10-30 22:18:49 +0800

    修复附加属性设置不正确的问题

--- demo/LuYao.Avalonia.Demo/ViewModels/VirtualizingWrapPanelViewModel.cs
+++ demo/LuYao.Avalonia.Demo/ViewModels/VirtualizingWrapPanelViewModel.cs
@@ -12,7 +12,7 @@ public class VirtualizingWrapPanelViewModel : ViewModelBase
         Items = new ObservableCollection<ItemViewModel>();
         
         // Create sample items
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 1000; i++)
         {
             Items.Add(new ItemViewModel
             {
